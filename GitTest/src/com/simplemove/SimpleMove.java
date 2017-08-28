da4bac8b17ba7853cb7848b649847a51a2624ab8
Luuk <Luuk@LuukPc.fritz.box>
2017-08-28 23:35:20 +0200

    Adding SimpleMove

---
+++ GitTest/src/com/simplemove/SimpleMove.java
@@ -0,0 +1,62 @@
+package com.simplemove;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import java.awt.*;
+
+public class SimpleMove extends JFrame {
+	
+	private InfoPanel infoPanel;
+	
+	public SimpleMove() {
+        
+        initUI();
+   }
+
+	private void initUI() {
+		
+		setLayout(new FlowLayout());
+		
+		Board board = new Board();
+		
+		infoPanel = new InfoPanel(board);
+		add(infoPanel);
+		
+		add(board);
+		
+		board.setInfoPanel(infoPanel);
+		
+		setSize(1000, 1000);
+		
+		setResizable(false);
+		pack();
+		
+//		setSize(Board.getBWidth() + InfoPanel.getIPWidth(),
+//				Board.getBHeight() + InfoPanel.getIPHeight());
+		
+		
+		
+		setTitle("SimpleMove");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		
+	}
+
+	
+
+	public static void main(String[] args) {
+
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+
+				SimpleMove game = new SimpleMove();
+				game.setVisible(true);
+			}
+		});
+	}
+}
